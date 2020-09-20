d24c04b61a8770999790389cf07252cfdf4e3b25
joshuamalek <71410106+joshuamalek@users.noreply.github.com>
2020-09-19 19:48:56 -0700

    asdf

--- form.php
+++ form.php
@@ -4,14 +4,17 @@
       <link rel="stylesheet" href="assets/css/pwform.css">
   </head>
   <body>
-  <div class="pwform-box">
-      <h1>Login Here</h1>
-  </div>
+
 
 <form method="POST">
   <?php if( $_SERVER['REQUEST_METHOD'] == 'POST' ) { ?>
     Invalid password
   <?php } ?>
+
+  <div class="pwform-box">
+      <h1>Login Here</h1>
+  </div>
+   
   <p>Enter password for access:</p>
   <input type="password" name="password">
   <button type="submit">Submit</button>
