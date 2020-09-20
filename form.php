d2a7e7d16d042c59b5cf59f89fda7f667a8bccc5
joshuamalek <71410106+joshuamalek@users.noreply.github.com>
2020-09-20 01:33:04 -0700

    adsf

--- form.php
+++ form.php
@@ -10,13 +10,13 @@
 
 <form method="POST">
   <?php if( $_SERVER['REQUEST_METHOD'] == 'POST' ) { ?>
-    That is not your name, User.
+    <div class="inc-pw">That is not your name, User.</div>
   <?php } ?>
 
   <div class="pwform-box">
-      <h1>You are User</h1>
+      <h1>If you are the User of the Program</h1>
       <div class="input-box">
-        <input type="password" name="password" placeholder="who are you?">
+        <input type="password" name="password" placeholder="then what is your name?">
       </div>
       <button type="submit" class="login-btn"> > </button>
   </div>
