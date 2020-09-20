1043a84c75682b704270f12056c7b268e3d28a97
joshuamalek <71410106+joshuamalek@users.noreply.github.com>
2020-09-19 20:09:39 -0700

    asdf

--- form.php
+++ form.php
@@ -15,7 +15,7 @@
       <h1>Login Here</h1>
       <div class="input-box">
       <p>Enter password for access:</p>
-      <input type="password" name="password">
+      <input type="password" name="password" placeholder="User's Name">
       <button type="submit">Submit</button>
     </div>
   </div>
